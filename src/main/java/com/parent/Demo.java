4dae836e4d9eeeea40afd443f4c90388ef293886
Venkatraman Muthukrishnan <vmuthukrishnan@informatica.com>
2024-11-21 14:06:36 +0530

    test

---
+++ src/main/java/com/parent/Demo.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+import com.parent.DemoParent;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        DemoParent.run(-2);
+        System.out.printf("Demo");
+    }
+}
